19f95ce94dd031750cd936e899738edc7db94d27
amanda-mac-air <amanda.mengdan.zhang@gmail.com>
2021-02-02 00:31:20 -0500

    20210201

--- 11-Container-With-Most-Water.py
+++ 11-Container-With-Most-Water.py
@@ -2,47 +2,47 @@ from typing import List
 
 
 class Solution:
-    # two pointer solution
+    """
+    two pointer solution
+    run time O(n), space O(1)
+    the area is determined by width * the shorter height
+    left pointer starts from head, right pointer starts from tail
+    now the question is how to move the pointers
+    Claim: suppose height[L] < height[R], if there is a better solution within [L, R], then [L_opt, R_opt] must in [L+1, R]
+    proof: Let [L_opt, R_opt] be the optimal solution in [L, R], area_opt > area, L_opt >= L, R_opt <= R
+           Assume [L_opt, R_opt] is not in [L+1, R] ==> L_opt = L, R_opt < R
+           so area_opt = (R_opt - L_opt) * min(height[L_opt], height[R_opt])
+                       = (R_opt - L) * min(height[L], height[R_opt])
+                       < (R - L) * height[L]
+                       = area
+           Contradiction!
+           So the optimal solution is guaranteed to be in [L+1, R], so we just need to move the pointer with shorter height
+    """
     def max_area(self, height: List[int]) -> int:
-        if len(height) == 0:
-            return 0
-
-        left = 0
-        right = len(height) - 1
-        min_height = min(height[left], height[right])
-        area = (right - left) * min_height
-
-        while left < right:
-            while left < right and height[left] <= min_height:
-                left += 1
-            if left == right:
-                break
-            min_height = min(height[left], height[right])
-            if (right - left) * min_height > area:
-                area = (right - left) * min_height
-
-            while left < right and height[right] <= min_height:
-                right -= 1
-            if left == right:
-                break
-            min_height = min(height[left], height[right])
-            if (right - left) * min_height > area:
-                area = (right - left) * min_height
-
+        l = 0
+        r = len(height) - 1
+        area = 0
+        while l < r:
+            if height[l] < height[r]:
+                area = max(area, (r - l) * height[l])
+                l += 1
+            else:
+                area = max(area, (r - l) * height[r])
+                r -= 1
         return area
 
 
 if __name__ == "__main__":
     solution = Solution()
     # empty input
-    print(solution.maxArea([]))
+    print(solution.max_area([]))
     # 2 vertical lines with same heights
-    print(solution.maxArea([1, 1]))
+    print(solution.max_area([1, 1]))
     # max width -> max area
-    print(solution.maxArea([1, 2, 1]))
+    print(solution.max_area([1, 2, 1]))
     # no 2 lines with none-zero height
-    print(solution.maxArea([0, 2, 0]))
+    print(solution.max_area([0, 2, 0]))
     # monotonic heights
-    print(solution.maxArea([1, 2, 3, 4, 5, 6]))
+    print(solution.max_area([1, 2, 3, 4, 5, 6]))
     # max width -\-> max area
-    print(solution.maxArea([3, 9, 3, 4, 7, 2, 12, 6]))
+    print(solution.max_area([3, 9, 3, 4, 7, 2, 12, 6]))
---
+++ 42-Trapping-Rain-Water.py
@@ -0,0 +1,79 @@
+from typing import List
+
+
+class Solution:
+    """
+    Brute force solution that takes O(n^2) time
+
+    total water = 0
+    for i = 1 : n
+        Let left_max(i) = height of the tallest building on the left side of i
+                        = max(H[0], ..., H[i])
+            right_max(i) = height of the tallest building on the right side of i
+                         = max(H[i], ..., H[n-1])
+        height of trapped water at i = min(left_max(i), right_max(i))
+        w_i (trapped water at i) = min(left_max(i), right_max(i)) - H[i]
+        total water += w_i
+
+    return total water
+    ---------------------------------------------------------------------------------------
+    Is there a better way to update left_max(i) and right_max(i)?
+
+    2 pointers solution: run time O(n), space O(1)
+    Let left pointer L starts from head, right pointer R starts from tail (L < R)
+
+    suppose height[L] < height[R]
+        left_max(L) = max(left_max(L-1), height[L])
+        since right_max[L] >= right_max[L] >= height[R], we have w_L = min(left_max(L), right_max(L)) - H[i] = left_max(L) - H[i]
+        as we've known w_L, we can move to L+1
+
+    suppose height[L] >= height[R]
+        right_max(R) = max(right_max(R+1), height[R])
+        since left_max[R] >= left_max[R] >= height[L], we have w_L = min(left_max(R), right_max(R)) - H[i] = right_max(R) - H[i]
+        as we've known w_R, we can move to R-1
+
+    We can update left_max(L) and right_max(R) in O(1) if we track left_max(L-1) and right_max(R+1)
+    """
+    def trap(self, height: List[int]) -> int:
+        l = 0
+        r = len(height) - 1
+        volume = 0
+        left_max = 0
+        right_max = 0
+
+        while l < r:
+            if height[l] < height[r]:
+                left_max = max(left_max, height[l])
+                volume += left_max - height[l]
+                l += 1
+            else:
+                right_max = max(right_max, height[r])
+                volume += right_max - height[r]
+                r -= 1
+        return volume
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    # smallest height in the middle
+    assert solution.trap([4, 2, 0, 3, 2, 5]) == 9
+
+    # largest height in the middle
+    assert solution.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]) == 6
+
+    # monotonic increasing heights -> no water trapped
+    assert solution.trap([1, 2, 3, 4, 10]) == 0
+
+    # monotonic decreasing heights -> no water trapped
+    assert solution.trap([10, 8, 6, 5]) == 0
+
+    # empty heights -> no water trapped
+    assert solution.trap([]) == 0
+
+    # all 0 heights -> no water trapped
+    assert solution.trap([0, 0]) == 0
+
+
+
+
+
